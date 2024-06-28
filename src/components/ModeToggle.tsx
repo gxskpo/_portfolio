81566d95bc0d1595f2f65bdcd25842cc32f4696c
haru <ggalactiko@galactiko.net>
2024-06-28 14:24:01 -0600

    Add dark mode toggle

---
+++ src/components/ModeToggle.tsx
@@ -0,0 +1,28 @@
+import { Moon, Sun } from "./icons";
+import { useState, useEffect } from "react";
+import styles from "../Home.module.css";
+
+export default function ModeToggle() {
+  const [darkMode, setDarkMode] = useState<boolean>(true);
+
+  useEffect(() => {
+    let darkmodeEnabled =
+      document.body.classList.contains("dark") ||
+      window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (!darkmodeEnabled) {
+      setDarkMode(false);
+    }
+  }, []);
+
+  const handler = () => {
+    document.body.classList.toggle("dark");
+    setDarkMode(!darkMode);
+  };
+
+  return (
+    <button onClick={handler} className={styles.topbarButton}>
+      {darkMode ? <Moon /> : <Sun />}
+    </button>
+  );
+}
